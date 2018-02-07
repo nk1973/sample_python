3337c12c77be7e82171e734b559ad639536cee4a
Nikolai Burtsev <nk1973@gmail.com>
2018-02-07 14:19:53 +0200

    format & replace logging with syslog

--- mqtt_client.py
+++ mqtt_client.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python
 
-import logging
+import syslog
+import logging.handlers
 import json
 import signal
 import time
+import os
 import paho.mqtt.client as mqtt
 
-LOGFILE = '/tmp/log'
 SEND_LOGS_TRIGGER = '/tmp/syslog_sendmail.now'
+PANEL_ID_FILE = '/tmp/panel_id'
+RETRIES_TO_WAIT_FOR_PANEL_ID_FILE = 30
 
 
 class Config:
@@ -29,28 +32,28 @@ class App:
     """Mqtt client class"""
 
     def __init__(self, panel_id, host, port, user, passwd):
-        signal.signal(signal.SIGTERM, self.handle)
-        signal.signal(signal.SIGINT, self.handle)
+        signal.signal(signal.SIGTERM, self.__sig_handle)
+        signal.signal(signal.SIGINT, self.__sig_handle)
 
         self.panel_id = panel_id
         self.client = mqtt.Client(
             self.panel_id,
             clean_session=True,
             userdata=self)
-        self.client.on_connect = App.on_connect
-        self.client.on_disconnect = App.on_disconnect
-        self.client.on_message = App.on_message
-        self.client.on_log = App.on_log
+        self.client.on_connect = App.__on_connect
+        self.client.on_disconnect = App.__on_disconnect
+        self.client.on_message = App.__on_message
+        self.client.on_log = App.__on_log
         self.client.message_callback_add(
             'to_panel/' +
             self.panel_id +
             '/cmds/push_log',
-            App.on_push_log)
+            App.__on_push_log)
         self.client.message_callback_add(
             'to_panel/' +
             self.panel_id +
             '/cmds/open_vpn',
-            App.on_open_vpn)
+            App.__on_open_vpn)
         self.client.username_pw_set(user, passwd)
 
         # the will message will be published on disceonnect from the broker
@@ -72,81 +75,94 @@ class App:
             payload='online',
             retain=True)
         self.__quit = False
-        return
 
     @staticmethod
-    def on_connect(client, app, flags, rc):
-        logging.debug('client:{} connected: {}'.format(app.panel_id, rc))
-        return
-
-    @staticmethod
-    def on_disconnect(client, app, flags, rc):
-        logging.debug('client:{} disconnect: {}'.format(app.panel_id, rc))
-        return
-
-    @staticmethod
-    def on_message(client, app, msg):
-        logging.debug(
-            'client:{} got msg of topic{}: payload: {}'.format(
+    def __on_connect(client, app, flags, rc):
+        syslog.syslog(
+            syslog.LOG_DEBUG,
+            'client:{} connected: {}'.format(
                 app.panel_id,
-                msg.topic,
-                msg.payload))
-        return
+                rc))
 
     @staticmethod
-    def on_push_log(client, app, msg):
-        logging.debug('client:{} push_log: payload {}'.format(
-            app.panel_id,
-            msg.payload))
+    def __on_disconnect(client, app, flags, rc):
+        syslog.syslog(
+            syslog.LOG_DEBUG,
+            'client:{} disconnect: {}'.format(
+                app.panel_id,
+                rc))
+
+    @staticmethod
+    def __on_message(client, app, msg):
+        syslog.syslog(syslog.LOG_DEBUG,
+                      'client:{} got msg of topic{}: payload: {}'.format(
+                          app.panel_id,
+                          msg.topic,
+                          msg.payload))
+
+    @staticmethod
+    def __on_push_log(client, app, msg):
+        syslog.syslog(
+            syslog.LOG_DEBUG,
+            'client:{} push_log: payload {}'.format(
+                app.panel_id,
+                msg.payload))
         # NOTE: we can use json data to define different log server settings
         # like host, port, encrypted, user, passwd. But now all this
         # is hardcoded
         trigger = open(SEND_LOGS_TRIGGER, 'w')
         trigger.write('{}'.format(time.time()))
-        logging.debug('written: {}'.format(time.time()))
+        syslog.syslog(syslog.LOG_DEBUG, 'written: {}'.format(time.time()))
         trigger.close()
-        return
 
     @staticmethod
-    def on_open_vpn(client, app, msg):
-        logging.debug(
-            'client:{} open_vpn payload: {}'.format(
-                app.panel_id,
-                msg.payload))
+    def __on_open_vpn(client, app, msg):
+        syslog.syslog(syslog.LOG_DEBUG,
+                      'client:{} open_vpn payload: {}'.format(
+                          app.panel_id,
+                          msg.payload))
         # FIXME: open vpn
-        return
 
     @staticmethod
-    def on_log(client, app, level, buf):
-        logging.debug('client: {}: {}'.format(app.panel_id, buf))
-        return
+    def __on_log(client, app, level, buf):
+        syslog.syslog(
+            syslog.LOG_DEBUG,
+            'client: {}: {}'.format(
+                app.panel_id,
+                buf))
 
-    def handle(self, signum, frame):
-        logging.debug('catched signal: {}'.format(signum))
+    def __sig_handle(self, signum, frame):
+        syslog.syslog(syslog.LOG_DEBUG, 'catched signal: {}'.format(signum))
         if signum == signal.SIGTERM or signum == signal.SIGINT:
-            self.__quit = True
+            self.stop()
 
     def stop(self):
         self.__quit = True
-        return
 
     def run(self):
         while not self.__quit:
             ret = self.client.loop()
             if ret != mqtt.MQTT_ERR_SUCCESS and not self.__quit:
                 self.client.reconnect()
-                logging.warning('reconnecting')
-        logging.debug('{} quit'.format(self.panel_id))
-        return
+                syslog.syslog(syslog.WARNING, 'reconnecting')
+        syslog.syslog(syslog.LOG_DEBUG, '{} quit'.format(self.panel_id))
 
 
 if __name__ == "__main__":
-    # FIXME: read panel id from filesystem
-    panel_id = '12345'
-    logging.basicConfig(
-        format='%(asctime)s:%(message)s',
-        filename=LOGFILE,
-        level=logging.DEBUG)
+    for i in range(RETRIES_TO_WAIT_FOR_PANEL_ID_FILE):
+        if not os.access(PANEL_ID_FILE, os.R_OK):
+            syslog.syslog(
+                syslog.WARNING,
+                'no panel id file {} exist for reading, waiting'.format(PANEL_ID_FILE))
+            time.sleep(1)
+        else:
+            break
+
+    # let throw exception in case no panel id file - watchdod will restart uo
+    fp = open(PANEL_ID_FILE, 'r')
+    panel_id = fp.read()
+    syslog.syslog(syslog.LOG_INFO, 'using panel id {}'.format(panel_id))
+
     cfg = Config('remote_config.json')
     cfg.dump()
     app = App(panel_id, cfg.host, cfg.port, cfg.user, cfg.passwd)
--- uart_logger.py
+++ uart_logger.py
@@ -2,74 +2,101 @@
 # Burtsev Nikolai
 # The program reads data from uart & saves it to logfile
 
-
 import serial
-import logging
+import signal
+import syslog
+import os
 
-LOGFILE = '/tmp/log'
-TIMEOUT_SEC = 5
+TIMEOUT_SEC = 4
 UART_PATH = '/dev/ttyUSB1'
 BAUDRATE = 115200
+PANEL_ID_FILE = '/tmp/panel_id'
 
 
 class UartReader:
     """reads uart data & saves it to system log"""
 
     def __init__(self, path, baudrate, the_timeout):
+        self.__quit = False
+        signal.signal(signal.SIGTERM, self.__sig_handler)
+        signal.signal(signal.SIGINT, self.__sig_handler)
+
         self.__uart = serial.Serial(path, baudrate, timeout=the_timeout)
         self.__uart.write('\r\n')
         self.__uart.flush()
         data = self.__uart.readline()
-        print('read len: {} '.format(len(data)))
-        if self.__is_in_debug_mode(data):
-            self.__quit_debug_mode()
-        elif len(data) > 2:
-            logging.info(data[:-2])
-        else:
-            got_data = False
-            for i in range(10):
-                self.__enable_logging()
-                data = self.__uart.readline()
-                if (len(data) > 2):  # \r\n
-                    logging.info(data[:-2])
-                    got_data = True
-                else:
-                    logging.warning('----no data from uart')
-            if not got_data:
-                raise RuntimeError('no data from uart')
-        return
+
+        # if no logging was enabled
+        if not data:
+            self.__enable_logging()
+            self.__uart.write('\r\n')
+            self.__uart.flush()
+            data = self.__uart.readline()
+        panel_id = self.__read_panel_id(self.__is_in_debug_mode(data))
+
+        # create panel id file
+        fp = open(PANEL_ID_FILE + '.tmp', 'w')
+        fp.write(panel_id)
+        fp.close()
+        os.rename(PANEL_ID_FILE + '.tmp', PANEL_ID_FILE)
+
+        print('panel id: {}'.format(panel_id))
+
+    def __sig_handler(self, signum, stack_frame):
+        if signum == signal.SIGINT or signum == signal.SIGTERM:
+            self.__quit = True
 
     def __is_in_debug_mode(self, data):
         return data.count('DEBUG>') > 0 or data.count(
             'Error: No such command') > 0 or data.count('Enter \'help\'') > 0
 
+    def __enter_debug_mode(self):
+        self.__uart.write('`\r\n')
+        self.__uart.flush()
+        print('entered debug mode')
+
     def __quit_debug_mode(self):
         self.__uart.write('quit\r\n')
         self.__uart.flush()
-        return
 
     def __enable_logging(self):
         self.__uart.write('quasar\r\n')
         self.__uart.flush()
-        return
+
+    def __read_panel_id(self, is_in_debug):
+        if not is_in_debug:
+            self.__enter_debug_mode()
+        self.__uart.write('ip\r\n')
+        self.__uart.flush()
+        read_panel_id = False
+        panel_id = ''
+        while not read_panel_id and not self.__quit:
+            data = self.__uart.readline()
+            if len(data) > 2 and data.count('IP: ') > 0:
+                idx = data.find('IP: ')
+                space_idx = data.find(' ', idx + 4)
+                panel_id = data[idx + 4:space_idx - 1]
+                read_panel_id = True
+        self.__quit_debug_mode()
+        return panel_id
 
     def run(self):
-        while True:
+        while not self.__quit:
             data = self.__uart.readline()
             if self.__is_in_debug_mode(data):
                 self.__quit_debug_mode()
             elif len(data) > 2:
-                logging.info(data[:-2])
+                syslog.syslog(syslog.LOG_INFO, data[:-2])
             else:
-                logging.warning('---timeout to read data from uart')
-        return
+                syslog.syslog(syslog.LOG_WARNING, '---timeout to read data')
+        syslog.syslog(syslog.LOG_INFO, 'uart reader quit.')
 
 
 if __name__ == "__main__":
-    logging.basicConfig(
-        format='%(message)s',
-        filename=LOGFILE,
-        level=logging.INFO)
+    #    logging.basicConfig(
+    #        format='%(message)s',
+    #        filename=LOGFILE,
+    #        level=logging.INFO)
     reader = UartReader(UART_PATH, BAUDRATE, TIMEOUT_SEC)
     reader.run()
 
